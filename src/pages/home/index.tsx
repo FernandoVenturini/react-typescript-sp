375e5ed310de26e840e69504db2f0c3245735877
Fernando <fernandoventurini9@hotmail.com>
2026-09-18 23:02:34 +0100

    login #78

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,8 +3,9 @@ import { Container } from "../../components/container";
 export function Home() {
     return (
         <Container>
+            {/* HEADER */}
             <section className="bg-gray-200 p-4 rounded-lg w-full max-w-3xl mx-auto flex justify-center items-center gap-2">
-                <input 
+                <input
                     className="w-full border-2 rounded-lg h-9 px-3 outline-none"
                     placeholder="Digite o nome do carro..."
                 />
@@ -14,73 +15,93 @@ export function Home() {
             </section>
 
             <h1 className="font-bold text-center mt-6 text-2xl mb-4">
-                Carros novos e usados em todo o Brasil
+                Carros novos e usados em todo o UK
             </h1>
 
+            {/* MAIN */}
             <main className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
 
+                {/* FIRST CARD */}
                 <section className="w-full bg-white rounded-lg">
                     <img
                         className="w-full rounded-lg mb-2 max-h-100 hover:scale-105 transition-all"
-                        src="https://image.webmotors.com.br/_fotos/anunciousados/gigante/2026/202608/20260812/bmw-x5-3.0-i6-turbo-hibrido-xdrive45e-m-sport-automatico-wmimagem11440365046.jpg?s=fill&w=242&h=190&q=70&type=webp" 
-                        alt="Carro" 
+                        src="https://m.atcdn.co.uk/a/media/w340/170831b23a9143468744b1cd1e0fd7cb.jpg"
+                        alt="Carro"
                     />
-                    <p className="font-bold mt-1 mb-2 px-2">BMW X5</p>
+                    <p className="font-bold mt-1 mb-2 px-2">Porsche Cayenne</p>
 
                     <div className="flex flex-col px-2">
-                        <span className="text-zinc-700 mb-6">Ano 2016/2016 | 23.000 km</span>
-                        <strong className="text-black font-medium text-xl">R$ 320.000</strong>
+                        <span className="text-zinc-700 mb-6">3.0h V6 S TiptronicS 4WD Euro 5 (s/s) 5dr</span>
+                        <span className="text-zinc-700 mb-6">MEGA SPEC | MERDAB BODYKIT | ULEZ</span>
+                        <div className="flex flex-row items-center gap-4 px-2/">
+                            <span className="text-zinc-700 mb-6 rounded-lg bg-gray-200 p-1">67.000 miles</span>
+                            <span className="text-zinc-700 mb-6 rounded-lg bg-gray-200 p-1">2011(60reg)</span>
+                        </div>
+                        <strong className="text-black font-medium text-xl">£320.000</strong>
                     </div>
 
                     <div className="w-full h-px bg-slate-200 my-2"></div>
 
                     <div className="px-2 pb-2">
-                        <span className="text-black">
-                            Sao Paulo - SP
+                        <span className="text-zinc-700">
+                            Keighley (72 miles)
                         </span>
                     </div>
                 </section>
 
+                {/* SECOND CARD */}
                 <section className="w-full bg-white rounded-lg">
                     <img
                         className="w-full rounded-lg mb-2 max-h-100 hover:scale-105 transition-all"
-                        src="https://m.atcdn.co.uk/a/media/w340/dc23a87b61d6407a9fd0836337fd6b0a.jpg" 
-                        alt="Carro" 
-                    />
-                    <p className="font-bold mt-1 mb-2 px-2">Volvo V90</p>
-
-                    <div className="flex flex-col px-2">
-                        <span className="text-zinc-700 mb-6">Ano 2020/2021 | 23.000 km</span>
-                        <strong className="text-black font-medium text-xl">R$ 520.000</strong>
-                    </div>
-
-                    <div className="w-full h-px bg-slate-200 my-2"></div>
-
-                    <div className="px-2 pb-2">
-                        <span className="text-black">
-                            Sao Paulo - SP
-                        </span>
-                    </div>
-                </section>
-
-                <section className="w-full bg-white rounded-lg">
-                    <img
-                        className="w-full rounded-lg mb-2 max-h-100 hover:scale-105 transition-all"
-                        src="https://m.atcdn.co.uk/a/media/w260/95e3b493fe63420783ea16ff1bf06b00.jpg" 
-                        alt="Carro" 
+                        src="https://m.atcdn.co.uk/a/media/w340/630f3c19df1a4da288018931e627c361.jpg"
+                        alt="Carro"
                     />
                     <p className="font-bold mt-1 mb-2 px-2">Porsche Taycan</p>
 
                     <div className="flex flex-col px-2">
-                        <span className="text-zinc-700 mb-6">Ano 2023/2023 | 23.000 km</span>
-                        <strong className="text-black font-medium text-xl">R$370.000</strong>
+                        <span className="text-zinc-700 mb-6">Performance 79.2kWh 4S Auto 4WD 4dr (11kW Charger)</span>
+                        <span className="text-zinc-700 mb-6">APPLECARPLAY | REAR CAMERA</span>
+                    </div>
+                    <div className="flex flex-row items-center gap-4 px-2/">
+                        <span className="text-zinc-700 mb-6 rounded-lg bg-gray-200 p-1">Lower Price</span>
+                        <span className="text-zinc-700 mb-6 rounded-lg bg-gray-200 p-1">17,471 miles</span>
+                        <span className="text-zinc-700 mb-6 rounded-lg bg-gray-200 p-1">2021(71reg)</span>
+                    </div>
+                    <strong className="text-black font-medium text-xl">£36.000</strong>
+
+                    <div className="w-full h-px bg-slate-200 my-2"></div>
+
+                    <div className="px-2 pb-2">
+                        <span className="text-zinc-700">
+                            Spalding (43 miles)
+                        </span>
+                    </div>
+                </section>
+
+                {/* THIRD CARD */}
+                <section className="w-full bg-white rounded-lg">
+                    <img
+                        className="w-full rounded-lg mb-2 max-h-100 hover:scale-105 transition-all"
+                        src="https://m.atcdn.co.uk/a/media/w340/1b4e357ac3274821a1e8004798f27aa9.jpg"
+                        alt="Carro"
+                    />
+                    <p className="font-bold mt-1 mb-2 px-2">Porsche Boxster</p>
+
+                    <div className="flex flex-col px-2">
+                        <span className="text-zinc-700 mb-6">2.7 987 2dr</span>
+                        <span className="text-zinc-700 mb-6">Stunning Lava Orange 2.7Manual</span>
+                        <div className="flex flex-row items-center gap-4 px-2/">
+                            <span className="text-zinc-700 mb-6 rounded-lg bg-gray-200 p-1">99.600 miles</span>
+                            <span className="text-zinc-700 mb-6 rounded-lg bg-gray-200 p-1">2007(07reg)</span>
+                        </div>
+                        <strong className="text-black font-medium text-xl">£9.950</strong>
                     </div>
 
                     <div className="w-full h-px bg-slate-200 my-2"></div>
 
                     <div className="px-2 pb-2">
-                        <span className="text-black">
-                            Sao Paulo - SP
+                        <span className="text-zinc-700">
+                            Ashford (108 miles)
                         </span>
                     </div>
                 </section>
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,69 @@
+import logoImg from '../../assets/logoImg.svg'
+import { Link } from 'react-router-dom'
+import { Container } from '../../components/container'
+import { Input } from '../../components/input'
+import { useForm} from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const schema = z.object({
+    email: z.string().email("Insira um email valido!").nonempty("O campo email e obrigatorio!"),
+    password: z.string().nonempty("O campo senha e obrigatorio!")
+})
+
+type FormData = z.infer<typeof schema>
+
+
 export function Login() {
+    const { register, handleSubmit, formState: { errors }} = useForm<FormData>({
+        resolver: zodResolver(schema),
+        mode: "onChange"
+    })
+
+    function onSubmit(data: FormData) {
+        console.log(data);
+    }
+
     return (
-        <div>
-            <h1>Pagina Login</h1>
-        </div>
+        <Container>
+            <div className="w-full min-h-screen flex justify-center items-center flex-col gap-4">
+                <Link to="/" className='b-6 max-w-sm w-full'>
+                    <img 
+                        src={logoImg} 
+                        alt="Logo do site"
+                        className='w-full' 
+                    />
+                </Link>
+
+                <form 
+                    className="bg-white max-w-xl w-full rounded-lg"
+                    onSubmit={handleSubmit(onSubmit)}
+                >
+                    <div className='mb-3'>
+                        <Input
+                            type="email"
+                            placeholder="Digite seu email..."
+                            name="email"
+                            error={errors.email?.message}
+                            register={register}
+                        />
+                    </div>
+
+                    <div className='mb-3'>
+                        <Input
+                            type="password"
+                            placeholder="Digite seu senha..."
+                            name="password"
+                            error={errors.password?.message}
+                            register={register}
+                        />
+                    </div>
+                    
+                    <button>
+                        Acessar
+                    </button>
+                </form>
+            </div>
+        </Container>
     )
 }
